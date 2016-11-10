0d4a0a170bab844822be99a1b131c0756a459707
Your Name <you@example.com>
2016-11-10 12:03:05 +0100

    Added test for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new
+  end
+
+  test "Email should be required" do
+    @user.valid?
+    assert_includes(@user.errors[:email], "can't be blank")
+  end
+
 end
