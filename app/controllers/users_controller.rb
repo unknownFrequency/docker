d50d36edc4cf3d838902889ba8e8f192d8ba7913
Your Name <you@example.com>
2016-11-14 10:40:42 +0100

    Gonna recover ibdata1 so mysql wont crash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,37 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        # Sends email to user when user is created.
+        Usermailer.email_token(@user).deliver
+
+        format.html { redirect_to 'users#index', notice: 'Check venligst din email'  }
+        format.json { render :index, status: :created, location: 'users#index'  }
+      else
+        format.html { render :new  }
+        format.json { render json: @user.errors, status: :unprocessable_entity  }
+      end
+      end
+              
   end
 
   def index
+    respond_with @user if @user
   end
 
   def update
   end
 
   def delete
+  end
+
+  def show 
+    respond_with @user if @user
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :username, :firstname, :lastname, :zip, :address)
   end
 end
