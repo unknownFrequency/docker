d1d0266e36b4f3b75f9efa26ad6ce2f94e8d28d5
Your Name <you@example.com>
2016-11-16 09:36:56 +0100

    Testet and implementet exp i token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,8 @@ class ApplicationController < ActionController::Base
     nil
   end
 
-  def self.encode(payload)
+  def self.encode(payload, exp = 1.month.from_now)
+    payload[:exp] = exp
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root 'home#index'
   post 'auth_user' => 'application#authenticate_request!'
-  post '/users/token', to: 'users#token'
+  #post '/users/token', to: 'users#token'
+  post '/users/:id/:token', to: 'users#token'
   get 'home'       => 'home#index'
   resources :users
+
+  #namespace :api do
+    #namespace :v1 do
+      #resources :users
+    #end
+  #end
 end
